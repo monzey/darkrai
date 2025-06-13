3a5e63d479283e508a4e42344c752fb70c50ced2
Maxime Bertrand <maxi.bertrand@gmail.com>
2025-06-14 00:15:23 +0200

    chore: add nix flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Environnement Python pour projet JDR-IA";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        pythonEnv = pkgs.python311.withPackages (ps: with ps; [
+          fastapi
+          uvicorn
+          openai
+          langchain
+          requests
+        ]);
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ pythonEnv pkgs.poetry ];
+
+          shellHook = ''
+            echo "🐍 Python JDR-IA environment ready!"
+            export PYTHONPATH=$(pwd)
+          '';
+        };
+      }
+    );
+}
